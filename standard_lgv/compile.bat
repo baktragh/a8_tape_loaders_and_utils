0b10bc11919ff6a2b2c62f188196e1656e4476fb
baktragh <zylon@post.cz>
2022-03-29 11:18:16 +0200

    Finish rename from lgv to lbe, merge with master

--- standard_lgv/compile.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-C:\utils\a8\mads\mads -d:LDRTYPE=0 -o:lgv.bot lgv.asm
-C:\utils\a8\mads\mads -d:LDRTYPE=1 -o:lgv.xex lgv.asm
-PAUSE
--- standard_lgv/compile_testfile.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-C:\utils\a8\mads\mads -o:testfile.xex testfile.asm
-PAUSE
