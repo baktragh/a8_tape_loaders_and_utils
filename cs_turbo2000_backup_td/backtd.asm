b9196453bca32f19a2093e3f835760d5f3902b3e
baktra <zylon@post.cz>
2024-04-09 16:20:18 +0200

    Backup T/D: Bootable disk prototype. Disk checks.

--- cs_turbo2000_backup_td/backtd.asm
+++ cs_turbo2000_backup_td/backtd.asm
@@ -8,12 +8,12 @@
 ;
 ;Minimum of 128 KB of total RAM is required, because the turbo block is loaded
 ;into the banks of the extended memory. This allows to process files that
-;are about 48 KB long.
+;are up to 64 KB long.
 ;*******************************************************************************
             ICL 'equates.asm'
             ICL 'auxmacs.mac'
 
-            OPT H+,F-
+            OPT H-,F-
             
 ;-------------------------------------------------------------------------------
 ; Local equates and definitions
@@ -24,17 +24,57 @@
             ZP_BASEBANK   EQU 128
             ZP_CURRBANK   EQU 129
             ZP_LASTBANK   EQU 130
+            ZP_RETCODE    EQU 131
             VBI_VCOUNT    EQU 124 
+            SEC_BUFFER    EQU $2000
+
+            SNO_MARKING   EQU 25
+            SNO_PRISTINE  EQU 26
+            SNO_DATA      EQU 27
+            
 ;-------------------------------------------------------------------------------
 ; Mainline code
 ;-------------------------------------------------------------------------------
             ORG PROGRAM_START
+            .byte $00
+            .byte [[PROGRAM_END-PROGRAM_START]/128]+1
+            .word PROGRAM_START
+            .word L05D2
 
 ;Initialize the copier
 L05D2       jsr CASINIT       ;Setup DOS vectors   
             jsr DISPLAY_TITLE ;Display the title
             jsr DISPLAY_START_TO_LOAD ;Press START prompt
             jsr WAIT_START    ;Wait for the START key
+            
+;Verify, if the disk is OK
+            jsr DISK_VERIFY_EYE
+            lda ZP_RETCODE
+            beq VERIFY1_OK
+
+            jsr DISPLAY_VERIFY1_FAILED
+VERIFY1_BAD lda #$12
+            sta COLOR4
+            jmp ENDLESS                
+
+VERIFY1_OK  jsr DISK_VERIFY_PRISTINE
+            lda ZP_RETCODE
+            beq VERIFY2_OK
+
+            jsr DISPLAY_VERIFY2_FAILED
+            jsr DISPLAY_START_OR_RESET
+            jsr WAIT_START
+            
+
+VERIFY2_OK  jsr DISK_VERIFY_WRITABLE
+            lda ZP_RETCODE
+            beq VERIFY3_OK
+
+            jsr DISPLAY_VERIFY3_FAILED
+            jsr DISPLAY_START_OR_RESET
+            jsr WAIT_START
+
+VERIFY3_OK
 
 ;Prepare the base PORTB value
             lda PORTB         ;Get current settings
@@ -330,9 +370,135 @@ L06FC       dec BRKKEY
 L06FE       clc
 L06FF       rts
 
+;===============================================================================
+; Disk operations
+;===============================================================================
 ;-------------------------------------------------------------------------------
-; Display name
+; Verify the marking sector (nr. 25)
 ;-------------------------------------------------------------------------------
+DISK_VERIFY_EYE
+            SUBENTRY
+            lda #0
+            sta ZP_RETCODE
+
+            lda #1
+            sta DUNIT 
+            lda #<SNO_MARKING
+            sta DAUX1     
+            lda #>SNO_MARKING
+            sta DAUX2
+            lda #$52     ; "R"
+            sta DCOMND
+            lda #>SEC_BUFFER
+            sta DBUFHI  
+            lda #<SEC_BUFFER 
+            sta DBUFLO
+            jsr DSKINV
+
+            lda DSTATS                ;Check the status
+            cmp #1                    ;Is status OK (==1)?
+            bne DVE_BAD               ;No, return 8
+
+DVE_COMPARE    
+            ldx #DVE_T_EYE_L
+@           lda SEC_BUFFER-1,X
+            cmp DVE_T_EYE-1,X
+            bne DVE_BAD
+            dex
+            bne @-
+DVE_DONE
+            SUBEXIT
+
+DVE_BAD     lda #8                    ;Set RC=8
+            sta ZP_RETCODE
+            jmp DVE_DONE
+
+
+DVE_T_EYE     dta c'TURGEN BACKUP T/D 1.00'
+DVE_T_EYE_L   equ *-DVE_T_EYE
+;-------------------------------------------------------------------------------
+; Verify if the disk is pristine / writable
+;-------------------------------------------------------------------------------
+DISK_VERIFY_PRISTINE
+            SUBENTRY
+            lda #0
+            sta ZP_RETCODE
+
+            lda #1
+            sta DUNIT 
+            lda #<SNO_PRISTINE
+            sta DAUX1     
+            lda #>SNO_PRISTINE
+            sta DAUX2
+            lda #$52     ; "R"
+            sta DCOMND
+            lda #>SEC_BUFFER
+            sta DBUFHI  
+            lda #<SEC_BUFFER 
+            sta DBUFLO
+            jsr DSKINV
+
+            lda DSTATS                ;Check the status
+            cmp #1                    ;Is status OK (==1)?
+            bne DVP_BAD               ;No, return 8
+
+DVP_COMPARE                           ;Check for all $55s
+            ldx #128
+@           lda SEC_BUFFER-1,X
+            cmp #$55
+            bne DVP_BAD               ;If value not expected, bad
+            dex
+            bne @-
+DVP_DONE
+            SUBEXIT
+
+DVP_BAD     lda #8                    ;Set RC=8
+            sta ZP_RETCODE
+            jmp DVP_DONE
+
+;-------------------------------------------------------------------------------
+; Verify if the disk is writable
+;-------------------------------------------------------------------------------
+DISK_VERIFY_WRITABLE
+            SUBENTRY
+
+            lda #0
+            sta ZP_RETCODE
+
+            ldx #128
+            lda #$C0
+@           sta SEC_BUFFER-1,X
+            dex
+            bne @-             
+ 
+            lda #1
+            sta DUNIT 
+            lda #<SNO_PRISTINE
+            sta DAUX1     
+            lda #>SNO_PRISTINE
+            sta DAUX2
+            lda #$57     ; Put sector, with verification
+            sta DCOMND
+            lda #>SEC_BUFFER
+            sta DBUFHI  
+            lda #<SEC_BUFFER 
+            sta DBUFLO
+            jsr DSKINV
+
+            lda DSTATS                ;Check the status
+            cmp #1                    ;Is status OK (==1)?
+            bne DVW_BAD               ;No, return 8
+
+DVW_DONE    SUBEXIT
+
+DVW_BAD     lda #8                    ;Set RC=8
+            sta ZP_RETCODE
+            jmp DVW_DONE            
+
+
+;===============================================================================
+; Display messages
+;===============================================================================
            CIO0_OP    EQU $0342
            CIO0_STAT  EQU $0343
            CIO0_BUFLO EQU $0344
@@ -343,7 +509,7 @@ L06FF       rts
            CIO0_AUX2  EQU $034B
 
 ;-------------------------------------------------------------------------------
-; Display name
+; Display Found: filename
 ;-------------------------------------------------------------------------------
 DISPLAY_FOUND
            SUBENTRY
@@ -366,8 +532,6 @@ DISPLAY_FOUND
 
 M_FOUND    dta c'Found: '
 M_FOUND_L  equ *-M_FOUND
-
-
 ;-------------------------------------------------------------------------------
 ; Display program title
 ;-------------------------------------------------------------------------------
@@ -383,11 +547,10 @@ DISPLAY_TITLE SUBENTRY
            jsr MSG_DISPLAY
 
            SUBEXIT
-M_TITLE    dta 125,c'TURGEN - BACKUP T/D 0.01'
+M_TITLE    dta 125,c'TURGEN - BACKUP T/D 0.02'
 M_TITLE_L  equ *-M_TITLE
-
 ;-------------------------------------------------------------------------------
-; Display PRESS START to begin loading
+; Display PRESS START to begin backup
 ;-------------------------------------------------------------------------------
 DISPLAY_START_TO_LOAD SUBENTRY
            jsr MSG_CLR
@@ -402,6 +565,23 @@ DISPLAY_START_TO_LOAD SUBENTRY
            SUBEXIT
 M_START_TO_LOAD    dta c'Press START to begin backup'
 M_START_TO_LOAD_L  equ *-M_START_TO_LOAD
+
+;-------------------------------------------------------------------------------
+; Display START or RESET
+;-------------------------------------------------------------------------------
+DISPLAY_START_OR_RESET SUBENTRY
+           jsr MSG_CLR
+
+           ldx #M_START_OR_RESET_L
+@          lda M_START_OR_RESET-1,X
+           sta MSG_BUF-1,X
+           dex
+           bne @-
+           jsr MSG_DISPLAY
+
+           SUBEXIT
+M_START_OR_RESET    dta c'START to continue or RESET to abort'
+M_START_OR_RESET_L  equ *-M_START_OR_RESET
 
 ;-------------------------------------------------------------------------------
 ; Display 'File loaded OK'
@@ -439,6 +619,64 @@ DISPLAY_LOAD_ERROR
 M_LOAD_ERROR    dta c'  Load Error'
 M_LOAD_ERROR_L  equ *-M_LOAD_ERROR
 
+
+;-------------------------------------------------------------------------------
+; Display 'Disk identification failed'
+;-------------------------------------------------------------------------------
+DISPLAY_VERIFY1_FAILED
+           SUBENTRY
+           jsr MSG_CLR
+           jsr MSG_DISPLAY
+
+           ldx #M_VERIFY1_FAILED_L
+@          lda M_VERIFY1_FAILED-1,X
+           sta MSG_BUF-1,X
+           dex
+           bne @-
+           jsr MSG_DISPLAY
+
+           SUBEXIT
+M_VERIFY1_FAILED    dta c'Disk identification failed. Reboot.'
+M_VERIFY1_FAILED_L equ *-M_VERIFY1_FAILED
+
+;-------------------------------------------------------------------------------
+; Display 'Disk not pristine'
+;-------------------------------------------------------------------------------
+DISPLAY_VERIFY2_FAILED
+           SUBENTRY
+           jsr MSG_CLR
+           jsr MSG_DISPLAY
+
+           ldx #M_VERIFY2_FAILED_L
+@          lda M_VERIFY2_FAILED-1,X
+           sta MSG_BUF-1,X
+           dex
+           bne @-
+           jsr MSG_DISPLAY
+
+           SUBEXIT
+M_VERIFY2_FAILED    dta c'Disk not pristine'
+M_VERIFY2_FAILED_L equ *-M_VERIFY2_FAILED
+
+;-------------------------------------------------------------------------------
+; Display 'Disk not pristine'
+;-------------------------------------------------------------------------------
+DISPLAY_VERIFY3_FAILED
+           SUBENTRY
+           jsr MSG_CLR
+           jsr MSG_DISPLAY
+
+           ldx #M_VERIFY3_FAILED_L
+@          lda M_VERIFY3_FAILED-1,X
+           sta MSG_BUF-1,X
+           dex
+           bne @-
+           jsr MSG_DISPLAY
+
+           SUBEXIT
+M_VERIFY3_FAILED   dta c'Disk not writable'
+M_VERIFY3_FAILED_L equ *-M_VERIFY3_FAILED
+
 ;-------------------------------------------------------------------------------
 ; Turbo header buffer
 ;-------------------------------------------------------------------------------
@@ -451,26 +689,27 @@ TH_RUN      dta 0,0
 TH_END      EQU *
 TH_CHKSUM   dta 0
 TH_FULL_LEN EQU *-THEADER
-
 ;===============================================================================
 ; Messaging support
 ;===============================================================================
 ;-------------------------------------------------------------------------------
 ; Message buffer
 ;-------------------------------------------------------------------------------
-MSG_BUF     dta c'12345678901234567890123456789012',$9B
+MSG_BUF     dta c'123456789012345678901234567890123456',$9B
+MSG_BUF_L   EQU *-MSG_BUF-1
 
 ;-------------------------------------------------------------------------------
 ; Clear message
 ;-------------------------------------------------------------------------------
-MSG_CLR    lda #32 
-           ldx #32
+MSG_CLR    SUBENTRY
+           lda #32 
+           ldx #MSG_BUF_L
 MSG_C_L1   sta MSG_BUF-1,X
            dex
            bne MSG_C_L1
-           rts              
+           SUBEXIT              
 ;-------------------------------------------------------------------------------
-; Display message buffer
+; Display message buffer using CIO, channel 0.
 ;-------------------------------------------------------------------------------
 MSG_DISPLAY SUBENTRY
            lda #9                  ;Requesting PRINT
@@ -479,28 +718,26 @@ MSG_DISPLAY SUBENTRY
            sta CIO0_BUFLO
            lda #>MSG_BUF
            sta CIO0_BUFHI
-           lda #33
+           lda #MSG_BUF_L+1
            sta CIO0_LENLO
            ldx #0                  ;Channel 0
            stx CIO0_LENHI
            jsr CIOV                ;Call CIO
-
            SUBEXIT
 ;===============================================================================
-; Miscellaneous
+; Miscellaneous routines
 ;===============================================================================
-
-;-----------------------------------------------------------------------
-; Wait for VBLANK
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+; Wait for VBLANK (using VCOUNT)
+;-------------------------------------------------------------------------------
 WAIT_FOR_VBLANK    SUBENTRY
                    lda #VBI_VCOUNT
 WFV_1              cmp VCOUNT
                    bne WFV_1
                    SUBEXIT 
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Short delay
-;-----------------------------------------------------------------------                   
+;-------------------------------------------------------------------------------                   
 SHORT_DELAY        SUBENTRY
                    ldy #220
 DELAY_LOOP_E       ldx #255            
@@ -521,6 +758,10 @@ S_LOOP2     lda CONSOL
             cmp #6
             bne S_LOOP2
             SUBEXIT
+;-------------------------------------------------------------------------------
+; Endless loop
+;-------------------------------------------------------------------------------
+ENDLESS     jmp ENDLESS 
 ;===============================================================================
 ;Initialization of DOS vectors
 ;===============================================================================
@@ -529,13 +770,13 @@ CASINIT     SUBENTRY
             sta COLDST                    
             lda #02                       ;Cassette boot successfull
             sta BOOT
-            lda #<PROGRAM_START             ;CASINI to loader entry
+            lda #<PROGRAM_START           ;CASINI to loader entry
             sta CASINI
             lda #>PROGRAM_START
             sta CASINI+1
             SUBEXIT
-;================================================================================
-; RUN Vector
-;================================================================================
-            ORG RUNAD
-            .word PROGRAM_START
+
+PROGRAM_END EQU *
+            .REPT 3072-[PROGRAM_END-PROGRAM_START]
+            .byte 0
+            .ENDR
--- cs_turbo2000_backup_td/compile.bat
+++ cs_turbo2000_backup_td/compile.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
 set PATH=%PATH%;C:\utils\a8\mads
-mads -l:backtd.lst -o:backtd.xex backtd.asm
+mads -l:build\atr1.lst -o:build\atr1.bin atr_header.asm
+mads -l:build\atr2.lst -o:build\atr2.bin atr_marking_pristine.asm
+mads -l:build\backtd.lst -o:build\backtd.bot backtd.asm
+mads -l:build\atrc.lst -o:build\backtd_0.atr atr_compose.asm
+copy /b build\backtd_0.atr+atr_filler.dat backtd.atr
 PAUSE
