ab58f541e4fae7e45ef349f660f15aae068071be
baktra <zylon@post.cz>
2024-04-29 15:25:37 +0200

    TSFX: Standard. Trying to implement customizable IRGs

--- tape_sfx_skeletons/standard/standard_sfx_skeleton.asm
+++ tape_sfx_skeletons/standard/standard_sfx_skeleton.asm
@@ -1,14 +1,14 @@
-;=======================================================================
+;===============================================================================
 ; Standard Self-extractor skeleton   
 ; Assemble with the MADS assembler
-;=======================================================================
+;===============================================================================
            
                  ICL "equates.asm" 
                  OPT H+,F-
 
-;=======================================================================
+;===============================================================================
 ; Private constants
-;=======================================================================
+;===============================================================================
                 START_ADDR      = 2800
                 
                 ZP_TAB_PTR_LO   = 128
@@ -23,14 +23,15 @@
                 ZP_SIO_LENHI    = 134
 
 ;Block flag constants
-                
-                STANDARD_PILOT_LONG     = 0x80;         ;20 s
-                STANDARD_PILOT_MEDIUM   = 0x40;         ;3  s
-;=======================================================================
+                STANDARD_PILOT_20_0     = 0x80;         ;20 s
+                STANDARD_PILOT_16_0     = 0x40;         ;16 s
+                STANDARD_PILOT_08_0     = 0x20;         ;8  s
+                STANDARD_PILOT_03_0     = 0x10;         ;3  s
+;===============================================================================
 ; INITITALIZATION CODE - Switches off the display, so that
 ; loading data into the screen memory does no harm. Also ensure
 ; that RESET results in cold start.
-;=======================================================================
+;===============================================================================
                    ORG  START_ADDR      
                    ldy #0
                    sty SDMCTL
@@ -47,9 +48,9 @@ IN_WBV_L           cmp VCOUNT
                    ORG INITAD
                    .WORD START_ADDR
             
-;=======================================================================
+;===============================================================================
 ; MAINLINE CODE
-;=======================================================================
+;===============================================================================
                    ORG START_ADDR
                    jmp ENTRY_ADDR
 ;-----------------------------------------------------------------------
@@ -102,10 +103,12 @@ READY_SAVE         lda #<DATA_TABLE        ;Reset the table and counter
                    bmi SKIP_START         ;If $80 (composite)
                    jsr WAIT_FOR_START     ;Wait for START key
 SKIP_START         jsr BEEP
-;-----------------------------------------------------------------------
-;From now on, disable interrupts and DMA, keep motor ON until the contents
-;is fully recorded.
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+;Initiate recording
+; - Motor ON
+; - Silence for file separation
+; - Setup POKEY for writing cassette frames with customized IRGs
+;-------------------------------------------------------------------------------
                    lda #52
                    sta PACTL
 
@@ -115,7 +118,8 @@ SKIP_START         jsr BEEP
                    ldy CFG_SEP_DURATION   ;Long separator
                    jsr DELAY_CUSTOM_Y     ;Make long separator
 NORM_SEP           jsr DELAY_SHORT        ;Make normal separator
-;-----------------------------------------------------------------------      
+                   jsr TAPE_INIT_POKEY
+;-------------------------------------------------------------------------------      
 SAVE_LOOP          ldy #0                 ;Get buffer range
                    lda (ZP_TAB_PTR_LO),Y
                    sta BUFRLO
@@ -145,14 +149,19 @@ SAVE_DOBLOCK       jsr WRITE_BLOCK
                    lda #5
                    adc ZP_TAB_PTR_LO
                    sta ZP_TAB_PTR_LO
-                   bcc SAVE_CONT
+                   bcc @+
                    inc ZP_TAB_PTR_HI  
-SAVE_CONT
-
+@
 SAVE_NEXTBLOCK     jmp SAVE_LOOP                ;Continue saving.
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+; Terminate the recording
+; - Motor off
+; - RESET POKEY
+;-------------------------------------------------------------------------------
 SAVE_TERM          lda #60
                    sta PACTL
+
+                   jsr TAPE_TERM_POKEY          ;Terminate POKEY
 
                    bit CFG_FLAGS                ;Check for composite($80)
                    bmi SAVE_QUIT                ;If composite, quit
@@ -165,12 +174,12 @@ SAVE_ALARM         jsr BEEP                     ;Three beeps for alarm
                    jsr BEEP
 SAVE_AGAIN         jmp READY_SAVE               ;Otherwise start over    
 SAVE_QUIT          rts          
-;=======================================================================
+;===============================================================================
 ; KEYBOARD SUBROUTINES
-;=======================================================================                   
-;-----------------------------------------------------------------------
+;===============================================================================                   
+;-------------------------------------------------------------------------------
 ; Wait for START 
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 WAIT_FOR_START     lda #8
                    sta CONSOL
 WFS_LOOP           lda CONSOL             ;What keys?  
@@ -179,12 +188,12 @@ WFS_LOOP           lda CONSOL             ;What keys?
                    bne WFS_LOOP
 WFS_DONE           rts
                    
-;=======================================================================
+;===============================================================================
 ; OTHER SUBROUTINES
-;=======================================================================                   
-;-----------------------------------------------------------------------
+;===============================================================================                   
+;-------------------------------------------------------------------------------
 ; Beep
-;-----------------------------------------------------------------------                   
+;-------------------------------------------------------------------------------                   
 BEEP 	           lda #0
                    sta AUDCTL
                    lda #$AF
@@ -198,18 +207,18 @@ BELL_1             jsr WAIT_FOR_VBLANK
                    stx AUDC1                     ;Reset AUDC1 and AUDF1
                    stx AUDF1
                    rts
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Wait for VBLANK
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 WAIT_FOR_VBLANK    lda #VBI_VCOUNT             ;Get the desired value
 WFV_1              cmp VCOUNT                  ;Check
                    bne WFV_1                   ;If equal, keep checking
 WFV_2              cmp VCOUNT
                    beq WFV_2                   
                    rts             
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Short delay
-;-----------------------------------------------------------------------                   
+;-------------------------------------------------------------------------------                   
 DELAY_SHORT        ldy #5                  ;Short delay, 0.1 sec
 DELAY_CUSTOM_Y     jmp DELAY_WAIT
 
@@ -218,19 +227,40 @@ DELAY_WAIT         jsr WAIT_FOR_VBLANK     ;Wait for VBLANK
                    bne DELAY_WAIT          ;Repeat until not zero
 DELAY_END          rts
 
-;=======================================================================
+;===============================================================================
 ; Write block of data
 ; Inputs:
 ; BUFRLO,BUFRHI  - Right before first byte
 ; BFENLO,BFENHI  - Last byte
-;=======================================================================
+;===============================================================================
 WRITE_BLOCK        
 
 ;We are going to use SIO to write raw tape blocks.
 ;We need to adjust the buffer range, so that we have buffer start
 ;and length, for the SIO call
 
+;Ensure correct IRG
+                   lda ZP_BLOCKFLAG
+                   tay 
+                   and #STANDARD_PILOT_20_0
+                   beq @+
+                   jsr TAPE_IRG_20_0
+                   jmp WB_RANGE
+@                  tya
+                   and #STANDARD_PILOT_16_0
+                   beq @+
+                   jsr TAPE_IRG_16_0
+@                  tya
+                   and #STANDARD_PILOT_08_0
+                   beq @+
+                   jsr TAPE_IRG_08_0
+@                  tya
+                   and #STANDARD_PILOT_03_0
+                   bne @+
+                   jsr TAPE_IRG_03_0     
 ;First calculate length.
+@
+WB_RANGE
                    sec
                    lda BFENHI
                    sbc BUFRHI
@@ -271,18 +301,151 @@ WRITE_BLOCK
                    lda ZP_SIO_LENHI
                    sta DBYTHI
           
-                   lda #$00               ;Short gaps between records
+                   lda #$80               ;Short gaps between records
                    sta DAUX2
           
                    jsr SIOV               ;Call SIO 
 
                    rts
-;=======================================================================
+;===============================================================================
+; Tape related subroutines
+;===============================================================================
+;-------------------------------------------------------------------------------
+;Initialize tape recording
+;-------------------------------------------------------------------------------
+TAPE_INIT_POKEY   lda SSKCTL                   
+	              and #$0f
+	              ora #$20
+	              ora #$08
+	              sta SSKCTL
+	              sta SKCTL
+
+                  ldx #9
+TIP_LOOP_REG      lda TIP_REGDATA-1,X
+                  sta AUDF1-1,X
+                  dex
+                  bne TIP_LOOP_REG
+                    
+TIP_NOISY_AUDIO	
+                  lda #$a8
+                  sta audc4
+                  lda #$10
+                  bit sskctl
+                  bne TIP_NOISY_DONE
+                  sta audc1
+                  sta audc2
+TIP_NOISY_DONE
+
+TIP_RESET_SERIAL_STATUS
+	              sta SKREST
+	              rts
+TIP_REGDATA
+                  dta	$05		;audf1
+                  dta	$a0		;audc1
+                  dta	$07		;audf2
+                  dta	$a0		;audc2
+                  dta	$cc		;audf3
+                  dta	$a0		;audc3
+                  dta	$05		;audf4
+                  dta	$a0		;audc4
+                  dta	$28		;audctl
+;-------------------------------------------------------------------------------
+; Terminate pokey
+;-------------------------------------------------------------------------------
+TAPE_TERM_POKEY   lda #0
+	              sta audc1
+	              sta audc2
+	              sta audc4
+                  rts 
+;-------------------------------------------------------------------------------
+; Variaus tape IRGs
+;-------------------------------------------------------------------------------
+TAPE_IRG_20_0    lda PAL
+                 and #$0F
+                 bne TIRG_20_N
+TIRG_20_P
+                 lda #<1000
+                 sta W_IRG_LO
+                 lda #>1000
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+
+TIRG_20_N        lda #<1200
+                 sta W_IRG_LO
+                 lda #>1200
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+;-------------------------------------------------------------------------------
+TAPE_IRG_16_0    lda PAL
+                 and #$0F
+                 bne TIRG_16_N
+TIRG_16_P
+                 lda #<787
+                 sta W_IRG_LO
+                 lda #>787
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+
+TIRG_16_N        lda #<945
+                 sta W_IRG_LO
+                 lda #>945
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+;-------------------------------------------------------------------------------
+TAPE_IRG_08_0    lda PAL
+                 and #$0F
+                 bne TIRG_08_N
+TIRG_08_P
+                 lda #<387
+                 sta W_IRG_LO
+                 lda #>387
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+
+TIRG_08_N        lda #<465
+                 sta W_IRG_LO
+                 lda #>465
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+;-------------------------------------------------------------------------------
+TAPE_IRG_03_0    lda PAL
+                 and #$0F
+                 bne TIRG_03_N
+
+TIRG_03_P        lda #<138
+                 sta W_IRG_LO
+                 lda #>138
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE
+TIRG_03_N
+                 lda #<165
+                 sta W_IRG_LO
+                 lda #>165
+                 sta W_IRG_HI
+                 jmp TAPE_IRG_CORE        
+;-------------------------------------------------------------------------------
+TAPE_IRG_CORE
+                 mwa #TAPE_COUNTDOWN_HANDLER CDTMA1
+	             ldy W_IRG_LO
+	             lda W_IRG_HI
+	             tax
+                 lda #1
+                 sta timflg
+                 jsr SETVBV
+	             lda:rne timflg
+	             rts
+;-------------------------------------------------------------------------------
+; Countdown handler
+;-------------------------------------------------------------------------------
+TAPE_COUNTDOWN_HANDLER
+	mva		#0	timflg
+	rts
+;===============================================================================
 ; DISPLAY DATA
-;=======================================================================
-;-----------------------------------------------------------------------
+;===============================================================================
+;-------------------------------------------------------------------------------
 ; Display list
-;-----------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 DLIST      .BYTE 112,112,112
            .BYTE 7+64,<LINE_NAME,>LINE_NAME
            .BYTE 112,112
@@ -290,19 +453,14 @@ DLIST      .BYTE 112,112,112
            .BYTE $30
            .BYTE 2+64,<LINE_INSTR,>LINE_INSTR,2
            .BYTE 65,<DLIST,>DLIST
-;=======================================================================
+;===============================================================================
 ; DATA AREAS
-;=======================================================================
-;=======================================================================
+;===============================================================================
+W_IRG_LO   .BYTE 0
+W_IRG_HI   .BYTE 0
+;===============================================================================
 ; Segment data table
-;=======================================================================
+;===============================================================================
             DATA_TABLE=*
-            dta <(BLOCK1-1),>(BLOCK1-1),<BLOCK1_e,>BLOCK1_E,$00
-            dta $FF,$FF,$FF,$FF,$FF 
             SFX_CAPACITY = 49152-DATA_TABLE-5-5-1
-            START = START_ADDR
-
-
-BLOCK1      dta 0x55,0x55,0xFC,1,2,3,4,5,7,8,9,10,11,12,13,14,15,16
-BLOCK1_E    equ *-1
-         
+            START = START_ADDR     
