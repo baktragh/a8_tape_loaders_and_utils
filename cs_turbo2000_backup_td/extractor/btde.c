44fce5d8e5ca6a80713529337c3f3b16da62a815
baktra <zylon@post.cz>
2024-05-14 13:32:10 +0200

    Backup T/D: Extractor: Extract to plain or xex files.

--- cs_turbo2000_backup_td/extractor/btde.c
+++ cs_turbo2000_backup_td/extractor/btde.c
@@ -1,11 +1,14 @@
 /* Backup T/D
    Extractor
 */
-
 #include <conio.h>
 #include <atari.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+
 
 unsigned char cExtractCAS;
 unsigned char cExtractXEX;
@@ -14,10 +17,28 @@ unsigned char cTargetDeviceLetter;
 unsigned char cTargetDeviceNumber;
 unsigned char sectorBuffer[128];
 
-
 #define SNO_MARKING   33
 #define SNO_PRISTINE  34
 #define SNO_DATA      35
+
+#define RC_OK        0
+#define RC_ERROR     8
+#define DSKINV_RC_OK 1
+
+#define RETRY_OK     0 
+#define RETRY_RETRY  1
+#define RETRY_SKIP   2
+#define RETRY_ABORT  3
+
+
+struct _turboHeader {
+    unsigned char type;
+    char turboName[10];
+    unsigned int load;
+    unsigned int length;
+    unsigned int run;
+};
+typedef struct _turboHeader turboHeader_t;
 
 
 void paintMenu();
@@ -26,16 +47,22 @@ void selectTargetDevice();
 unsigned char readSector(void* buffer,unsigned char unitNumber,unsigned int sectorNumber);
 void listDisk();
 void handleDiskError(unsigned char returnCode);
+void handleFileError(unsigned char errorCode);
+void extractFiles();
+unsigned char verifyDisk(unsigned char unitNumber);
+unsigned char normalizeTurboName(char *turboName,unsigned char maxLength);
+unsigned char retryPrompt(char* message);
+unsigned char writeFile(char* name, unsigned int fileSize, unsigned int firstSector,unsigned char unitNumber,turboHeader_t *tHeader);
 
 int main() {
 
 char menuKey = ' ';
 
 /*Initialize default configuration*/
-cExtractCAS='Y';
+cExtractCAS='N';
 cExtractXEX='Y';
 cSourceDrive='1';
-cTargetDeviceLetter='D';
+cTargetDeviceLetter='H';
 cTargetDeviceNumber='1';
 
 /*Pain main menu and clear key input*/
@@ -59,7 +86,7 @@ if (menuKey=='b' || menuKey=='B') {
         continue;
     }
 /*Flip tape image extraction*/
-else if (menuKey=='c' || menuKey=='C') {
+/*else if (menuKey=='c' || menuKey=='C') {
         if (cExtractCAS=='Y') {
           cExtractCAS='N';
          }
@@ -67,7 +94,7 @@ else if (menuKey=='c' || menuKey=='C') {
           cExtractCAS='Y';
          }
         continue;
-    }
+}*/
 
 else if (menuKey=='l' || menuKey=='L') {
 listDisk();
@@ -79,8 +106,21 @@ else if (menuKey>='1' && menuKey<='8') {
    continue;
 }
 
+/*Target device selection*/
 else if (menuKey=='t' || menuKey=='T') {
     selectTargetDevice();
+}
+
+/*File extraction*/
+else if  (menuKey=='e' || menuKey=='E') {
+    if (cExtractXEX=='Y' || cExtractCAS=='Y') {
+       extractFiles();
+    }
+}
+
+/*Exit*/
+else if (menuKey=='q' || menuKey=='Q') {
+    break;
 }
 
 }
@@ -101,26 +141,21 @@ cursor(0);
 
 
 /*Title*/
-gotoxy(2,0);
-cputs("Backup T/D Extractor 0.01");
-gotoxy(2,1);
-cputs("(c) 2024 BAKTRA Software");
+
+cprintf("  Backup T/D Extractor 0.02\r\n");
+cprintf("  (c) 2024 BAKTRA Software\r\n\r\n");
 
 /*Menu items*/
-gotoxy(2,3);
-cputs("Main Menu");
-gotoxy(2,5);
-cputs("L   List files on Backup T/D disk");
-gotoxy(2,6);
-cputs(".   Extract files from disk");
-gotoxy(2,7);
-cprintf("B   Toggle binary file extaction [%c]",cExtractXEX);
-gotoxy(2,8);
-cprintf("C   Toggle tape image extraction [%c]",cExtractCAS);
-gotoxy(2,10);
-cprintf("1-8 Select BACKUP T/D disk drive [%c]",cSourceDrive);
-gotoxy(2,11);
-cprintf("T   Select target device [%c%c]",cTargetDeviceLetter,cTargetDeviceNumber);
+
+cprintf("  Main Menu\r\n\r\n");
+cprintf("  1-8 Select BACKUP T/D disk drive [%c]\r\n",cSourceDrive);
+cprintf("  L   List files on Backup T/D disk\r\n\r\n");
+cprintf("  T   Select target device [%c%c]\r\n",cTargetDeviceLetter,cTargetDeviceNumber);
+cprintf("  E   Extract files from disk\r\n\r\n");
+cprintf("  B   Toggle binary file extaction [%c]\r\n",cExtractXEX);
+cprintf("  .   Toggle tape image extraction [%c]\r\n\r\n",cExtractCAS);
+cprintf("  Q   Quit\r\n");
+
 
 }
 
@@ -180,48 +215,39 @@ else if (menuKey==CH_ENTER) {
 
 }
 
-/*List contents of the disk*/
-void listDisk() {
+/*Extract files from the disk*/
+void extractFiles() {
 unsigned char unitNumber = cSourceDrive-'0';
-unsigned char rc = 0;
-char nameBuffer[11];
-char sizeBuffer[5];
-unsigned int fileSize;
+unsigned char rc=0;
+char turboNameBuffer[11];
+char nameBuffer[3+10+1+3+1];
 unsigned int currentSector;
+unsigned int baseSector;
+unsigned int len;
+unsigned char retryCode;
+turboHeader_t headerCopy;
+char* extPtr;
 unsigned int numSectors;
 unsigned int remainderBytes;
 
-/*Put zero terminators to strings*/
-nameBuffer[10]=0;
-sizeBuffer[4]=0;
-
+/*Tell that we are extracting files*/
 clrscr();
-cputs("  Listing disk...\r\n\r\n");
+cputs("  Extracting files...\r\n");
 
-/*Read sector with marking*/
-rc = readSector(&sectorBuffer,unitNumber,SNO_MARKING);
-
-if (rc!=1) {
-  handleDiskError(rc);
-  return;
+/*Verify the disk*/
+if (verifyDisk(unitNumber)!=RC_OK) {
+return;
 }
 
-/*Check for the marking*/
-if (memcmp("TURGEN BACKUP T/D 1.00",&sectorBuffer,22)!=0) {
-  cputs("  Not a BACKUP T/D disk. Press any key.");
-  cgetc();
-  return; 
-}
-
-/*Position to the first sector with data*/
+/*Navigate to the first data sector*/
 currentSector = SNO_DATA;
 
-/*Now perform the listing*/
+/*Do this for all files*/
 while(1) {
 
-/*Read the sector*/
+/*Read the 'H'eader sector*/
 rc = readSector(&sectorBuffer,unitNumber,currentSector);
-if (rc!=1) {
+if (rc!=DSKINV_RC_OK) {
   handleDiskError(rc);
   return;
 }
@@ -234,18 +260,258 @@ if (sectorBuffer[0]!='H' && sectorBuffer[0]!='E') {
    return;
 }
 
+/*If end marker found, extraction is complete*/
+if (sectorBuffer[0]=='E') break;
+
+/*Copy the header, and copy the turbo name*/
+memcpy(&headerCopy,sectorBuffer+3,sizeof(turboHeader_t));
+memcpy(turboNameBuffer,&(headerCopy.turboName),10);
+turboNameBuffer[10]=0;
+
+/*Remember sector where the file began*/
+baseSector=currentSector+1;
+
+/*Prepare file name*/
+nameBuffer[0]=cTargetDeviceLetter;
+nameBuffer[1]=cTargetDeviceNumber;
+nameBuffer[2]=':';
+
+/*Normalize the turbo name*/
+len = normalizeTurboName(turboNameBuffer,cTargetDeviceLetter=='H'?10:8);
+
+/*Place the normalized name to the buffer*/
+memcpy(nameBuffer+3,turboNameBuffer,len);
+
+/*Place file extension*/
+switch (headerCopy.type) {
+   case 0x03:
+   case 0x04:
+     extPtr=".XEX";
+     break;
+   case 0xFF:
+   case 0xFE:
+     extPtr=".BAS";
+     break;
+   default:
+     extPtr=".DAT";
+     break;
+}
+memcpy(nameBuffer+3+len,extPtr,4);
+
+/*Place termination character*/
+nameBuffer[3+len+4]=0;
+
+/*Print the file name and all information from the header*/
+cprintf("\r\n  %-10s %02X $%04X $%04X $%04X\r\n",turboNameBuffer,headerCopy.type,headerCopy.load, headerCopy.run, headerCopy.length);
+cprintf("  ->%s\r\n",nameBuffer);
+
+/*Try to write the file*/
+extract_retry:
+rc = writeFile(nameBuffer,headerCopy.length,baseSector,unitNumber,&headerCopy);
+if (rc!=RC_OK) {
+  retryCode = retryPrompt("Extraction failed.");
+}
+else {
+  retryCode = RETRY_OK;
+}
+
+/*Based on result, decide how to continue*/
+/*Extraction aborted, just terminate*/
+if (retryCode==RETRY_ABORT) {
+  cputs("  Extraction aborted. Press any key.\r\n");
+  cgetc();
+  return;
+}
+
+/*Retry writing the file*/
+if (retryCode==RETRY_RETRY) {
+   goto extract_retry;
+}
+
+/*Now either the extraction was ok, or file was skipped*/
+if (retryCode==RETRY_SKIP) {
+   cputs("  Extraction skipped\r\n");
+}
+
+/*Navigate to the beginning of the next file and continue*/
+numSectors = (headerCopy.length+3)/128;
+remainderBytes = (headerCopy.length+3)%128;
+currentSector=baseSector+numSectors;
+if (remainderBytes!=0) currentSector++;
+
+} /*For all disk entries*/
+
+/*Report that extraction is complete*/
+cputs("\r\n  Extraction complete. Press any key.\r\n");
+cgetc();
+
+}
+
+/*Write file*/
+unsigned char writeFile(char* name, unsigned int fileSize, unsigned int firstSector, unsigned char unitNumber,turboHeader_t *tHeader) {
+
+unsigned int currentSector=firstSector;
+unsigned int bytesToGo=fileSize;
+unsigned char firstFlag=1;
+unsigned int rc;
+unsigned int  sectorByteCount;
+void* bufPtr;
+unsigned int lastAddr;
+unsigned char segmentBuffer[6];
+
+FILE* f=0;
+
+/*Try to open the file*/
+f = fopen(name,"wb");
+if (f==NULL) {
+    handleFileError(errno);
+    return RC_ERROR;
+}
+
+/*Do we need to encapsulate in a binary file?*/
+if (tHeader->type==0x03) {
+
+    segmentBuffer[0]=0xFF;
+    segmentBuffer[1]=0xFF;
+    /*First address*/
+    segmentBuffer[2]=(tHeader->load&0x00FF);
+    segmentBuffer[3]=(tHeader->load>>8);
+    /*Last address*/
+    lastAddr=tHeader->load+fileSize-1;
+    segmentBuffer[4]=(lastAddr&0x00FF);
+    segmentBuffer[5]=(lastAddr>>8);
+    
+    /*Write the header*/
+    rc=fwrite(segmentBuffer,1,6,f);
+    if (rc!=6) {
+        handleFileError(errno);
+        goto wf_term_bad;
+    }
+}
+
+/*Now keep reading the sectors untill all bytes are read*/
+while (bytesToGo>0) {
+
+    /*Read a sector*/
+    rc=readSector(&sectorBuffer,unitNumber,currentSector);
+    if (rc!=DSKINV_RC_OK) {
+      handleDiskError(rc);
+      goto wf_term_bad;
+    }
+   
+    /*First sector holds 'D' and requires special treatment*/
+    if (firstFlag==1) {
+       firstFlag=0;
+       if (sectorBuffer[0]!='D') {
+       cputs ("  Sector mark D not found\r\n");
+       goto wf_term_bad;
+       }
+       bufPtr=sectorBuffer+3;
+       sectorByteCount=128-3;
+    }
+    /*Other sectors hold up to 128 bytes*/
+    else {
+       bufPtr=sectorBuffer;
+       sectorByteCount=128;
+    }
+    
+    /*Count how many bytes we really need to get*/
+    if (bytesToGo<sectorByteCount) {
+       sectorByteCount=bytesToGo;
+    }
+
+    rc=fwrite(bufPtr,1,sectorByteCount,f);
+    if (rc!=sectorByteCount) {
+       handleFileError(errno);
+       goto wf_term_bad;
+    }
+
+    bytesToGo-=sectorByteCount;
+    currentSector++;
+}
+
+/*Write RUN segment, if needed*/
+if (tHeader->type==0x03) {
+
+    segmentBuffer[0]=0xE0;
+    segmentBuffer[1]=0x02;
+    segmentBuffer[2]=0xE1;
+    segmentBuffer[3]=0x02;
+
+    /*Run address*/
+    segmentBuffer[4]=((tHeader->run)&0x00FF);
+    segmentBuffer[5]=((tHeader->run)>>8);
+    
+    /*Write the segment*/
+    rc=fwrite(segmentBuffer,1,6,f);
+    if (rc!=6) {
+       handleFileError(errno);
+       goto wf_term_bad;
+    }
+}
+
+    fclose(f);
+    return RC_OK;
+
+wf_term_bad:
+    if (f!=0) fclose(f);
+    return RC_ERROR;
+}
+
+/*List contents of the disk*/
+void listDisk() {
+unsigned char unitNumber = cSourceDrive-'0';
+unsigned char rc = 0;
+unsigned int currentSector;
+unsigned int numSectors;
+unsigned int remainderBytes;
+char turboNameBuffer[11];
+turboHeader_t headerCopy;
+unsigned int fileSize;
+
+clrscr();
+cputs("  Listing disk...\r\n\r\n");
+
+/*Verify the disk. If not our disk, return*/
+if (verifyDisk(unitNumber)!=RC_OK) {
+return;
+}
+
+
+/*Position to the first sector with data*/
+currentSector = SNO_DATA;
+
+/*Now perform the listing*/
+while(1) {
+
+/*Read the sector*/
+rc = readSector(&sectorBuffer,unitNumber,currentSector);
+if (rc!=DSKINV_RC_OK) {
+  handleDiskError(rc);
+  return;
+}
+
+/*Check if the sector begins with 'H' or 'E'*/
+if (sectorBuffer[0]!='H' && sectorBuffer[0]!='E') {
+   cputs("  Sector mark H or E not found.\r\n");
+   cputs("  Press any key.\r\n");
+   cgetc();
+   return;
+}
+
 /*If end marker found, listing is complete*/
 if (sectorBuffer[0]=='E') break;
 
-/*Get file name*/
-memcpy(nameBuffer,sectorBuffer+4,10);
-
+/*Get header and name*/
+memcpy(&headerCopy,sectorBuffer+3,sizeof(turboHeader_t));
+memcpy(turboNameBuffer,&(headerCopy.turboName),10);
+turboNameBuffer[10]=0;
 
 /*Go to the first data sector*/
 ++currentSector;
 /*Read the data sector*/
 rc = readSector(&sectorBuffer,unitNumber,currentSector);
-if (rc!=1) {
+if (rc!=DSKINV_RC_OK) {
   handleDiskError(rc);
   return;
 }
@@ -253,17 +519,16 @@ if (rc!=1) {
 /*Check if the marking is 'D'*/
 if (sectorBuffer[0]!='D') {
    cputs("  Sector mark D not found.\r\n");
-   cputs("  Press any key.");
+   cputs("  Press any key.\r\n");
    cgetc();
    return;
 }
 
 /*Get the file size*/
 fileSize=sectorBuffer[1]+256*sectorBuffer[2];
-itoa(fileSize,sizeBuffer,16);
 
-/*Print file name and file size*/
-cprintf("  %10s $%04X\r\n",nameBuffer,fileSize);
+/*Display information on the file*/
+cprintf("  %-10s %02X $%04X $%04X $%04X\r\n",turboNameBuffer,headerCopy.type,headerCopy.load, headerCopy.run, headerCopy.length);
 
 /*Calculate the next header sector*/
 numSectors = (fileSize+3)/128;
@@ -274,16 +539,41 @@ if (remainderBytes!=0) currentSector++;
 }
 
 
-cputs("  Listing complete. Press any key\r\n");
+cputs("\r\n  Listing complete. Press any key.\r\n");
 clearKeyInput();
 cgetc();
 
 }
 
 void handleDiskError(unsigned char returnCode) {
-
 cprintf("\r\n  Disk I/O Error $%02X. Press any key.\r\n",returnCode);    
 cgetc();
+}
+
+void handleFileError(unsigned char errorCode) {
+cprintf("\r\n  File I/O Error $%04X.\r\n",errorCode);    
+}
+
+/*Verify if the disk is BACKUP T/D disk*/
+unsigned char verifyDisk(unsigned char unitNumber) {
+unsigned char rc;
+
+/*Read sector with marking*/
+rc = readSector(&sectorBuffer,unitNumber,SNO_MARKING);
+
+if (rc!=1) {
+  handleDiskError(rc);
+  return RC_ERROR;
+}
+
+/*Check for the marking*/
+if (memcmp("TURGEN BACKUP T/D 1.00",&sectorBuffer,22)!=0) {
+  cputs("  Not a BACKUP T/D disk. Press any key.\r\n");
+  cgetc();
+  return RC_ERROR; 
+}
+
+return RC_OK;
 
 }
 
@@ -303,6 +593,61 @@ __asm__ (" pla ");
 return OS.dcb.dstats;
 }
 
+unsigned char normalizeTurboName(char *buffer,unsigned char maxLength) {
+unsigned char length=maxLength;
+int i;
+unsigned char c;
+
+
+/*Find the real valid length - blanks on right do not count*/
+for (i=(maxLength-1);i>=0;i--) {
+  if ((buffer[i]&0x7F)!=' ') {
+     length=i+1;
+     break;
+  }
+}
+
+/*Normalize the name*/
+for (i=0;i<length;i++) {
+ 
+ c=buffer[i];
+ if (c&0x80) c=c&0x7F;
+ if (c==' ') c='_';
+ if ((c>='a' && c<='z') || (c>='!' && c<='_')) {
+ }
+ else {
+    c='_';
+ } 
+ 
+ buffer[i]=c;
+}
+
+ buffer[length]=0;
+
+ return length;
+
+}
+
+unsigned char retryPrompt(char* message) {
+  unsigned char c=' ';
+
+  cputs("\r\n  ");
+  cputs(message);
+  cputs("\r\n  R Retry, A Abort, S Skip\r\n");
+
+  while(1) {
+    c = cgetc();
+    if (c=='R' || c=='r') {
+      return RETRY_RETRY;
+    }
+    if (c=='A' || c=='a' || c==CH_ESC) {
+      return RETRY_ABORT;
+    }
+    if (c=='S' || c=='s') {
+      return RETRY_SKIP;
+    }
+  }
+}
 
 void clearKeyInput() {
 
