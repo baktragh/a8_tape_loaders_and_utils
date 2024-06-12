cb61215563aecb3151e313738ccb1fb22352afee
baktra <zylon@post.cz>
2024-06-12 10:30:39 +0200

    Backup T/D: Removed from the repository. New repository: https://github.com/baktragh/a8_backup_td

--- cs_turbo2000_backup_td/compile_extractor.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-cl65 -t atari --start-addr 8192 -o backup_td_extractor.xex btde.c
-PAUSE
--- cs_turbo2000_backup_td/resources/atr_compose_s.asm
+++
@@ -1,6 +0,0 @@
-                    ORG 0
-                    OPT h-f+
-                    INS 'build\atr1_s.bin'
-                    INS 'build\backtd.bot'
-                    INS 'build\atr2.bin'
-
